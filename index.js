2abf461265bc4862ecc576ff445894c6be53aea1
Harsh <rawalharsh48@gmail.com>
2019-06-15 18:49:17 -0400

    Heroku Deploying first step to victory

---
+++ index.js
@@ -0,0 +1,36 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: 'Hellow',
+    author: 'World',
+  },
+  {
+    title: 'Hello1',
+    author: 'world1',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
